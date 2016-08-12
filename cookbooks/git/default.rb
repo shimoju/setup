6eb8b57527e626aefb5fdf7df13de15ca88e8a59
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-12 19:57:19 +0900

    .gitconfigを設置

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -1,3 +1,14 @@
 package 'git'
 package 'hub'
 package 'ghq'
+
+credential_helper = node[:platform] == 'darwin' ? 'osxkeychain' : 'cache --timeout 36000'
+
+template File.expand_path('.gitconfig', '~') do
+  mode '644'
+  variables(credential_helper: credential_helper)
+end
+
+template File.expand_path('.gitignore_global', '~') do
+  mode '644'
+end
--- spec/localhost/git_spec.rb
+++ spec/localhost/git_spec.rb
@@ -9,3 +9,12 @@ require 'spec_helper'
     it { should be_installed }
   end
 end
+
+%w(
+  .gitconfig
+  .gitignore_global
+).each do |conf|
+  describe file(File.expand_path(conf, '~')) do
+    it { should be_file }
+  end
+end
