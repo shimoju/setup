722852aaf209fcc414a211bcb8ce6b3199e0910a
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-12 21:19:58 +0900

    ~/.sshディレクトリを作成

---
+++ cookbooks/ssh/default.rb
@@ -0,0 +1,3 @@
+directory File.expand_path('.ssh', '~') do
+  mode '700'
+end
--- spec/localhost/ssh_spec.rb
+++ spec/localhost/ssh_spec.rb
@@ -1,1 +1,6 @@
 require 'spec_helper'
+
+describe file(File.expand_path('.ssh', '~')) do
+  it { should be_directory }
+  it { should be_mode 700 }
+end
