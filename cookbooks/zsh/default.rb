8d4572278ad75052d844ca3ec923a54852b01139
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-12 23:53:23 +0900

    preztoをインストール

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -1,1 +1,7 @@
 package 'zsh'
+package 'git'
+
+git File.expand_path('.zprezto', '~') do
+  repository 'https://github.com/sorin-ionescu/prezto.git'
+  recursive true
+end
--- spec/localhost/zsh_spec.rb
+++ spec/localhost/zsh_spec.rb
@@ -3,3 +3,11 @@ require 'spec_helper'
 describe package('zsh') do
   it { should be_installed }
 end
+
+describe file(File.expand_path('.zprezto', '~')) do
+  it { should be_directory }
+end
+
+describe file(File.expand_path('.zprezto/init.zsh', '~')) do
+  it { should be_file }
+end
