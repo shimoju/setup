bd3bcdc12455cd529cd8c3182dcd6857f25b1d91
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-12 20:16:39 +0900

    .railsrcを設置

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -1,3 +1,7 @@
 template File.expand_path('.gemrc', '~') do
   mode '644'
 end
+
+template File.expand_path('.railsrc', '~') do
+  mode '644'
+end
--- spec/localhost/ruby_spec.rb
+++ spec/localhost/ruby_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 %w(
   .gemrc
+  .railsrc
 ).each do |conf|
   describe file(File.expand_path(conf, '~')) do
     it { should be_file }
