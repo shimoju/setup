571979483261b5a973553bf93d8086d41d4faa54
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-13 00:12:56 +0900

    preztoの設定ファイルをコピー

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -5,3 +5,16 @@ git File.expand_path('.zprezto', '~') do
   repository 'https://github.com/sorin-ionescu/prezto.git'
   recursive true
 end
+
+%w(
+  .zlogin
+  .zlogout
+  .zpreztorc
+  .zprofile
+  .zshenv
+  .zshrc
+).each do |conf|
+  template File.expand_path(conf, '~') do
+    mode '644'
+  end
+end
--- spec/localhost/zsh_spec.rb
+++ spec/localhost/zsh_spec.rb
@@ -11,3 +11,16 @@ end
 describe file(File.expand_path('.zprezto/init.zsh', '~')) do
   it { should be_file }
 end
+
+%w(
+  .zlogin
+  .zlogout
+  .zpreztorc
+  .zprofile
+  .zshenv
+  .zshrc
+).each do |conf|
+  describe file(File.expand_path(conf, '~')) do
+    it { should be_file }
+  end
+end
