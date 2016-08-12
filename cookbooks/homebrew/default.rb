9ffc36af909d35f9b682a83e304e6ef533cea4d3
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-12 18:18:10 +0900

    brew tapリポジトリをクックブックに書くようにして、テストを追加

--- cookbooks/homebrew/default.rb
+++ cookbooks/homebrew/default.rb
@@ -1,1 +1,10 @@
+node.reverse_merge!(
+  brew: {
+    add_repositories: [
+      'homebrew/dupes',
+      'homebrew/versions',
+    ]
+  }
+)
+
 include_recipe 'homebrew::common'
--- spec/localhost/homebrew_spec.rb
+++ spec/localhost/homebrew_spec.rb
@@ -4,3 +4,12 @@ describe file('/usr/local/bin/brew') do
   it { should be_file }
   it { should be_executable }
 end
+
+%w(
+  homebrew/dupes
+  homebrew/versions
+).each do |repo|
+  describe command("brew tap | grep -q #{repo}") do
+    its(:exit_status) { should eq 0 }
+  end
+end
