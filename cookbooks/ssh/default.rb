a92796cc9dc1276d93f2eb722f80874316f4bc72
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-12 22:12:12 +0900

    ~/.ssh/.configを設置

--- cookbooks/ssh/default.rb
+++ cookbooks/ssh/default.rb
@@ -3,3 +3,7 @@ package 'openssh'
 directory File.expand_path('.ssh', '~') do
   mode '700'
 end
+
+template File.expand_path('.ssh/config', '~') do
+  mode '600'
+end
--- spec/localhost/ssh_spec.rb
+++ spec/localhost/ssh_spec.rb
@@ -8,3 +8,8 @@ describe file(File.expand_path('.ssh', '~')) do
   it { should be_directory }
   it { should be_mode 700 }
 end
+
+describe file(File.expand_path('.ssh/config', '~')) do
+  it { should be_file }
+  it { should be_mode 600 }
+end
