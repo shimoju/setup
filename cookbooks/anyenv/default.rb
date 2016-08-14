fbc0e684feed6e0718462306478728d7267f4200
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-14 16:33:47 +0900

    anyenvをインストール

---
+++ cookbooks/anyenv/default.rb
@@ -0,0 +1,5 @@
+package 'git'
+
+git File.expand_path('~/.anyenv') do
+  repository 'https://github.com/riywo/anyenv.git'
+end
--- spec/localhost/anyenv_spec.rb
+++ spec/localhost/anyenv_spec.rb
@@ -1,1 +1,10 @@
 require 'spec_helper'
+
+describe file(File.expand_path('~/.anyenv')) do
+  it { should be_directory }
+end
+
+describe file(File.expand_path('~/.anyenv/bin/anyenv')) do
+  it { should be_file }
+  it { should be_executable }
+end
