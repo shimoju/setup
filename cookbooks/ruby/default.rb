ba449d15e28687d01e9cc4fba1c517585d7ff79e
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-15 20:28:40 +0900

    Rubyのビルドに必要なライブラリにlibyamlが抜けていたので追加

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -1,5 +1,6 @@
 include_recipe '../anyenv/default'
 
+package 'libyaml'
 package 'openssl'
 package 'readline'
 
--- spec/localhost/ruby_spec.rb
+++ spec/localhost/ruby_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 %w(
+  libyaml
   openssl
   readline
 ).each do |pkg|
