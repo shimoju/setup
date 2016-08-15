911c5f27651855fcf75e0999e2258039a1fcd18d
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-15 11:12:40 +0900

    rbenvのテストをまとめる

--- spec/localhost/ruby_spec.rb
+++ spec/localhost/ruby_spec.rb
@@ -19,11 +19,13 @@ end
   end
 end
 
-describe file(File.expand_path('~/.anyenv/envs/rbenv')) do
-  it { should be_directory }
-end
+describe 'rbenv' do
+  describe file(File.expand_path('~/.anyenv/envs/rbenv')) do
+    it { should be_directory }
+  end
 
-describe file(File.expand_path('~/.anyenv/envs/rbenv/bin/rbenv')) do
-  it { should be_file }
-  it { should be_executable }
+  describe file(File.expand_path('~/.anyenv/envs/rbenv/bin/rbenv')) do
+    it { should be_file }
+    it { should be_executable }
+  end
 end
