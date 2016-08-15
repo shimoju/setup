915b67e1ccbc194ea218089598b0e65373ee0421
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-15 09:56:51 +0900

    anyenv-updateを追加

--- cookbooks/anyenv/default.rb
+++ cookbooks/anyenv/default.rb
@@ -3,3 +3,7 @@ package 'git'
 git File.expand_path('~/.anyenv') do
   repository 'https://github.com/riywo/anyenv.git'
 end
+
+git File.expand_path('~/.anyenv/plugins/anyenv-update') do
+  repository 'https://github.com/znz/anyenv-update.git'
+end
--- spec/localhost/anyenv_spec.rb
+++ spec/localhost/anyenv_spec.rb
@@ -8,3 +8,12 @@ describe file(File.expand_path('~/.anyenv/bin/anyenv')) do
   it { should be_file }
   it { should be_executable }
 end
+
+describe file(File.expand_path('~/.anyenv/plugins/anyenv-update')) do
+  it { should be_directory }
+end
+
+describe file(File.expand_path('~/.anyenv/plugins/anyenv-update/bin/anyenv-update')) do
+  it { should be_file }
+  it { should be_executable }
+end
