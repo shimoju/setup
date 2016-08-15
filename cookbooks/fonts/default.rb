7152083738cc37b6325d64a18c55a3e8c03fc65e
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-15 16:50:11 +0900

    caskroom/fontsでフォントをインストール

--- cookbooks/fonts/default.rb
+++ cookbooks/fonts/default.rb
@@ -1,7 +1,20 @@
 include_recipe '../homebrew/default'
 
+node.reverse_merge!(
+  brew: {
+    install_fonts: [],
+  }
+)
+
 repo = 'caskroom/fonts'
 execute "Add Repository: #{repo}" do
   command "brew tap #{repo}"
   not_if "brew tap | grep '^#{repo}$'"
+end
+
+node['brew']['install_fonts'].each do |font|
+  execute "Install font: #{font}" do
+    command "brew cask install #{font}"
+    not_if "brew cask list | grep '^#{font}$'"
+  end
 end
