1cd7112eb3c23c292d5cc51c3d98c941c7a501d6
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-08-16 23:01:59 +0900

    Docker関連のaliasを設定

--- cookbooks/zsh/default.rb
+++ cookbooks/zsh/default.rb
@@ -47,6 +47,7 @@ end
 
 %w(
   .zprofile.d/anyenv.zsh
+  .zshrc.d/docker.zsh
   .zshrc.d/neovim.zsh
   .zshrc.d/ruby.zsh
 ).each do |conf|
---
+++ cookbooks/zsh/templates/.zshrc.d/docker.zsh
@@ -0,0 +1,5 @@
+alias dk='docker'
+# dcは逆ポーランド記法の計算機コマンドで存在する
+alias dc='docker-compose'
+alias dcu='docker-compose up -d --remove-orphans'
+alias dm='docker-machine'
