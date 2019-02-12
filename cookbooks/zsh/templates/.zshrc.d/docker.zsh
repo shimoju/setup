a0bd9a13c3789bcd2341b6a50e118045ccdf9e1e
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2019-02-12 16:28:07 +0900

    docker関連のalias追加

--- cookbooks/zsh/templates/.zshrc.d/docker.zsh
+++ cookbooks/zsh/templates/.zshrc.d/docker.zsh
@@ -1,7 +1,11 @@
 (( ! $+commands[docker] )) && return 1
 
 alias dk='docker'
+alias dkr='docker run --rm -it'
 # dcは逆ポーランド記法の計算機コマンドで存在する
 alias dc='docker-compose'
 alias dcu='docker-compose up -d --remove-orphans'
+alias dce='docker-compose exec'
+alias dcr='docker-compose run --rm'
+alias dcl='docker-compose logs -f --tail=100'
 alias dm='docker-machine'
