2951a798ee8cc244b6a4533ac5832d18bf18b11c
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2019-02-12 16:30:47 +0900

    anyenvのリポジトリ変更

--- cookbooks/anyenv/default.rb
+++ cookbooks/anyenv/default.rb
@@ -1,7 +1,7 @@
 package 'git'
 
 git File.expand_path('~/.anyenv') do
-  repository 'https://github.com/riywo/anyenv.git'
+  repository 'https://github.com/anyenv/anyenv.git'
 end
 
 git File.expand_path('~/.anyenv/plugins/anyenv-update') do
